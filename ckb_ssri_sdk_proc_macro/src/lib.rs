189fdf4286781614544c3321986f5f86dcbcf82b
Alive24 <xct24@live.com>
2025-01-08 23:35:07 +0000

    feat: updating hasMethods and getMethods

--- ckb_ssri_sdk_proc_macro/src/lib.rs
+++ ckb_ssri_sdk_proc_macro/src/lib.rs
@@ -27,166 +27,6 @@ fn encode_u64_vector(val: impl AsRef<[u64]>) -> Vec<u8> {
         .collect()
 }
 
-// Struct to hold method metadata for reflection and dispatch
-struct SSRIMethodMetadata {
-    pub method_name: String,
-    pub namespace: String,
-    pub method_signature: String,
-    pub method_attributes: SSRIMethodAttributes,
-}
-
-#[derive(Debug)]
-enum SSRIMethodLevel {
-    Code,
-    Script,
-    Cell,
-    Transaction,
-}
-
-impl Default for SSRIMethodLevel {
-    fn default() -> Self {
-        SSRIMethodLevel::Code
-    }
-}
-
-#[derive(Debug)]
-struct SSRIMethodAttributes {
-    pub implemented: bool,
-    pub internal: bool,
-    pub transaction: bool,
-    pub level: SSRIMethodLevel,
-}
-
-impl Default for SSRIMethodAttributes {
-    fn default() -> Self {
-        SSRIMethodAttributes {
-            implemented: true,
-            internal: false,
-            transaction: false,
-            level: SSRIMethodLevel::Code,
-        }
-    }
-}
-
-impl Parse for SSRIMethodAttributes {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-struct SSRIModuleAttributes {
-    pub version: String,
-    pub base: Option<String>,
-}
-
-impl Default for SSRIModuleAttributes {
-    fn default() -> Self {
-        SSRIModuleAttributes {
-            version: (&"0").to_string(),
-            base: None,
-        }
-    }
-}
-
-impl Parse for SSRIModuleAttributes {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-enum SSRISDKProcMacroError {
-    InvalidMethodAttribute,
-    InvalidModuleAttribute,
-    InvalidTraitName,
-}
-
-// Function to extract the trait name (used as the namespace if `base` is not provided)
-fn extract_trait_name(impl_block: &ItemImpl) -> Result<String, SSRISDKProcMacroError> {
-    if let Some((_, path, _)) = &impl_block.trait_ {
-        if let Some(segment) = path.segments.last() {
-            return Ok(segment.ident.to_string());
-        }
-    }
-    Err(SSRISDKProcMacroError::InvalidTraitName)
-}
-
-#[proc_macro_attribute]
-pub fn ssri_method(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let method_args = parse_macro_input!(attr as SSRIMethodAttributes);
-
-    let method = parse_macro_input!(item as ItemFn);
-
-    let method_metadata_const_name = format_ident!("__SSRIMETHOD_METADATA_{}", method.sig.ident);
-
-    // Create a metadata struct to represent the parsed attributes
-    let generated_method_metadata = quote! {
-        const #method_metadata_const_name: SSRIMethodMetadata = SSRIMethodMetadata {
-            namespace: "", // This will be set in ssri_module
-            method_name: #method.sig.ident.to_string(),
-            method_signature: #method.sig.to_string(),
-            method_attributes: ssri_method_attributes
-        };
-    };
-
-    // Return the method and the constant metadata
-    let expanded = quote! {
-        #method
-        // #generated_method_metadata
-    };
-
-    // Return the modified method as a TokenStream
-    TokenStream::from(expanded)
-}
-
-#[proc_macro_attribute]
-pub fn ssri_module(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemImpl);
-
-    let ssri_module_attributes = parse_macro_input!(attr as SSRIModuleAttributes);
-
-    let trait_name = extract_trait_name(&input).unwrap();
-
-    // Determine namespace based on `base` or fall back to the trait name
-    let namespace = match ssri_module_attributes.base {
-        Some(base) => base,
-        None => trait_name.clone(),
-    };
-    // let mut dispatch_cases = Vec::new();
-
-    // let mut module_metadata = Vec::new();
-
-    // for item in &impl_block.items {
-    //     if let ImplItemFn(method) = item {
-    //         let method_name = &method.sig.ident;
-    //         let method_name_str = method_name.to_string();
-    //         let args_types = method.sig.inputs.iter().map(|input| {
-    //             if let FnArg::Typed(pat) = input {
-    //                 pat.ty.to_token_stream().to_string()
-    //             } else {
-    //                 panic!("Invalid method signature");
-    //             }
-    //         });
-            
-    //         // Generate a match arm for each method
-    //         dispatch_cases.push(quote! {
-    //             #method_name_str => self.#method_name(param),
-    //         });
-    //     }
-    // }
-
-    // let generated_module_metadata = quote! {
-    //     const #module_metadata_const_name: &[SSRIMethodMetadata] = &[#(#module_metadata),*];
-    // };
-
-    let expanded = quote! {
-        #input
-        // #generated_module_metadata
-    };
-
-    TokenStream::from(expanded)
-}
 
 fn method_path(name: impl AsRef<[u8]>) -> u64 {
     u64::from_le_bytes(blake2b_256(name)[0..8].try_into().unwrap())
@@ -275,32 +115,50 @@ pub fn ssri_methods(input: TokenStream) -> TokenStream {
             ) {
                 #version_path => Ok(Cow::from(&[0][..])),
                 #get_methods_path => {
-                    let offset = usize::min((4 +u64::from_le_bytes(
+                    let offset = usize::min((u64::from_le_bytes(
                         decode_hex(&(#argv)[1])?
                             .try_into()
                             .map_err(|_| #invalid_args)?
-                    ) as usize * 8), #raw_methods_len);
-                    let limit = usize::min((4 + (offset + u64::from_le_bytes(
+                    ) as usize * 8), #raw_methods_len - 4);
+                    
+                    // If second argument is 0, take all remaining methods
+                    let second_arg = u64::from_le_bytes(
                         decode_hex(&(#argv)[2])?
                             .try_into()
                             .map_err(|_| #invalid_args)?
-                    ) as usize) * 8), #raw_methods_len);
-                    if limit == 0 {
-                        Ok(Cow::from(&RAW_METHODS[offset..]))
+                    );
+                    
+                    let mut raw_result: Vec<u8>;
+                    if second_arg == 0 {
+                        // Take all remaining methods from offset
+                        raw_result = RAW_METHODS[(offset + 4)..].to_vec();
+                        let method_count = (RAW_METHODS.len() - (offset + 4)) / 8;
+                        let mut result = (method_count as u32).to_le_bytes().to_vec();
+                        result.extend_from_slice(&raw_result);
+                        Ok(Cow::from(result))
                     } else {
-                        Ok(Cow::from(&RAW_METHODS[offset..limit]))
+                        let limit = usize::min((offset + (second_arg as usize * 8)), #raw_methods_len - 4);
+                        raw_result = RAW_METHODS[(offset + 4)..(limit + 4)].to_vec();
+                        let method_count = (limit - offset) / 8;
+                        let mut result = (method_count as u32).to_le_bytes().to_vec();
+                        result.extend_from_slice(&raw_result);
+                        Ok(Cow::from(result))
                     }
                 },
-                #has_methods_path => Ok(Cow::from(
-                    decode_hex(&(#argv)[1])?[4..].chunks(8).map(|path| {
+                #has_methods_path => {
+                    let mut result = Vec::new();
+                    let matches = decode_hex(&(#argv)[1])?[4..].chunks(8).map(|path| {
                         match RAW_METHODS[4..]
                             .chunks(8)
                             .find(|v| v == &path) {
                                 Some(_) => 1,
                                 None => 0,
                             }
-                    }).collect::<Vec<_>>()
-                )),
+                    }).collect::<Vec<_>>();
+                    result.extend_from_slice(&(matches.len() as u32).to_le_bytes());
+                    result.extend_from_slice(&matches);
+                    Ok(Cow::from(result))
+                },
                 #(
                     #method_keys => #method_bodies,
                 )*
@@ -309,11 +167,4 @@ pub fn ssri_methods(input: TokenStream) -> TokenStream {
             res
         }
     })
-}
-
-
-#[proc_macro]
-pub fn ssri_wasm(input: TokenStream) -> TokenStream {
-    // TODO: Export serializeStruct
-    todo!()
 }
